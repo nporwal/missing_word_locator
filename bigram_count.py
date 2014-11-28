cda7d21daa554a0113f38eaaa56da932e654abb2
luketjohnston <ltj22@cornell.edu>
2014-11-28 17:12:21 -0500

    Now, specify the percent of the training data to use, instead of the lines per step (it only does one step now).

--- bigram_count.py
+++ bigram_count.py
@@ -2,12 +2,11 @@ import pickle
 import sys
 import pymongo
 
-# Notes: 30 million lines, 2.2 million unique words
+# Call with the first argument separation, the second argument the percent of the 
+# training data to use.
 
-# Call with the first argument the separation, the second
-# argument 1000000. That value for the second argument just
-# splits the whole thing into three steps, so no single
-# step takes all of (my computer's) memory.
+# Total number of lines in the file. Might be off by one or two but who cares.
+NUM_LINES = 30301027
 
 ''' This program is used to pickle a dictionary of the frequencies
 of long-distance bigrams in the training set. It takes two arguments:
@@ -15,13 +14,14 @@ the separation of the bigrams and the number of lines to train on and
 save in one step. Counting and pickling in more than one step 
 speeds up the process.  '''
 
-
 # The separation between the two words in the bigram. The number of 
 # other words between the end words in a bigram of separation s is s-1.
 s = int(sys.argv[1])
 
-# The number of lines to process in each step (in each pickled dictionary)
-step_size = int(sys.argv[2])
+# The percent of the training data to train on. Must be greater than 0 and
+# less than or equal to 100.
+percent = float(sys.argv[2])
+
 # Database stuff
 # Database!
 client = pymongo.MongoClient()
@@ -46,37 +46,18 @@ print 'Reading lines...'
 line_num = 0;
 try:
     for l in train:
+        if 1.0*line_num/NUM_LINES > percent: break
         # For our algorithm to take into account how close words are to
         # the beginning and the end of the sentence, we must prepend and
         # append placeholder words.
         prepend = ['_before_' + str(s-i) for i in range(s)]
-        append = ['_after_' + str(i) for i in range(s)]
+        append = ['_after_' + str(i+1) for i in range(s)]
         word_list = prepend + l.lower().split() + append
         if s==1: 
             totals['_total_'] += len(l.split()) #update total count of all words
         line_num += 1
         if line_num%100000 == 0:
             print 'reading line ' + str(line_num)
-        if line_num % step_size == 0:
-            i = 0
-            print 'Entries to save: ' + str(len(d))
-            print 'Making bulk operation:'
-            bulk = collection.initialize_unordered_bulk_op()
-            for bigram in d.keys():
-                if (i % 5000 == 0):
-                    print '    Entry number: ' + str(i)
-                i += 1;
-                (word1,word2) = bigram.split()
-                # The first argument to update means to update the
-                # document that matches {'word1':word1,...}
-                bulk.find({'w1':word1,'w2':word2}).upsert().update( 
-                    # This increments the count of the bigram
-                    {'$inc':{'c':d[bigram]}})
-            print 'Calling bulk.execute...'
-            bulk.execute()
-            print 'Done!'
-            d.clear() # Now clear the dictionary to free up memory for the next part.
-            print '    Done!'
         for i in range(len(word_list)-s):
             first_word = word_list[i]
             if s==1: # Increment total count of this word.
@@ -85,30 +66,31 @@ try:
             second_word = word_list[i + s]
             try:
                 # increment bigram count by one
-                d[first_word + ' ' + second_word] += 1
+                d[(first_word,second_word)] += 1
             except KeyError:
-                d[first_word + ' ' + second_word] = 1
+                d[(first_word,second_word)] = 1
 except KeyboardInterrupt:
     pass
 
 i = 0
+print 'Writing to database...'
 print 'Entries to save: ' + str(len(d))
+#print 'Making bulk operation:'
+#bulk = collection.initialize_unordered_bulk_op()
 for bigram in d.keys():
-    if (i % 5000 == 0):
-        print '    Entry number: ' + str(i)
+    if (i % 10000 == 0):
+        print 'Entry number: ' + str(i)
     i += 1;
-    (word1,word2) = bigram.split()
-    collection.update({'word1':word1,'word2':word2},
-        {'$inc':{'count':d[bigram]}},
-         #'$setOnInsert': {'count':d[bigram]}},
-         upsert=True)
-d.clear()
-print '    Done!'
+    collection.insert({'w1':bigram[0],'w2':bigram[1],'c':d[bigram]})
+#print 'Calling bulk.execute...'
+#bulk.execute()
+d.clear() 
+print 'Done!'
 
 # The totals dictionary is small enough that it doesn't have to
 # be updated in steps.
-print 'Saving totals:'
 if s==1:
+    print 'Saving totals:'
     # Need to save the total counts into a collection
     totals_collection = db.totals
     
@@ -120,9 +102,3 @@ if s==1:
             print 'Word number: ' + str(i)
         i += 1;
         totals_collection.insert({'word':w, 'count':totals[w]})
-
-
-    
-
-
-
